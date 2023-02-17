9242198f09fcfa0ae6a7478e4b1e281bc6343eac
borshyovkirill <borshyovkirill@gmail.com>
2023-02-17 12:42:09 +0300

    fix tests

---
+++ src/shared/lib/tests/renderWithRouter/renderWithRouter.tsx
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import { ReactNode } from 'react';
+import { I18nextProvider } from 'react-i18next';
+import { MemoryRouter } from 'react-router-dom';
+import i18nForTests from 'shared/config/i18n/i18nForTests';
+
+export interface renderWithRouterProps {
+    route: string;
+}
+
+export function renderWithRouter(component: ReactNode, options: renderWithRouterProps) {
+    const { route } = options;
+    return render(
+        <MemoryRouter>
+            <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
+        </MemoryRouter>,
+    );
+}
